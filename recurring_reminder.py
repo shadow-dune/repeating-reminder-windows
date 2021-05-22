bb904dcdcdfd40793525e735edc5d4360f59319e
dyk123456789 <69682411+dyk123456789@users.noreply.github.com>
2021-05-22 17:17:13 +0530

    Use this file while copy-pasting code

--- recurring_reminder.py
+++ recurring_reminder.py
@@ -1,11 +1,12 @@
-# show a notification every 1500 seconds to take a break
 from win10toast import ToastNotifier
 from time import sleep
 
 toast = ToastNotifier()
 
 while True:
+    
     #Edit this line to change the title and description of the notification
     toast.show_toast("Reminder", "Take a break!")
+    
     #Edit this line to change the delay (in seconds)
     sleep(1500)
