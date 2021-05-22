2a611d8372711d1aac93709b90d8a4fd53329060
dyk123456789 <69682411+dyk123456789@users.noreply.github.com>
2021-05-22 17:06:38 +0530

    Update and rename recurring_timer.py to recurring_reminder.py

--- recurring_reminder.py
+++ recurring_reminder.py
@@ -5,5 +5,7 @@ from time import sleep
 toast = ToastNotifier()
 
 while True:
+    #Edit this line to change the title and description of the notification
     toast.show_toast("Reminder", "Take a break!")
+    #Edit this line to change the delay (in seconds)
     sleep(1500)
